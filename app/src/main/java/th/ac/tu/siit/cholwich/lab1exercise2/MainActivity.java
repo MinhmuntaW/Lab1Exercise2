48546de4d1846d4d377ae12084e7cfb1e088451a
student <yok_Havemail@hotmail.com>
2015-01-19 15:30:09 +0700

    Ex.2 calculate Temperature

--- app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -14,6 +18,57 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    public void convertClicked(View v){
+        EditText etInput =(EditText)findViewById(R.id.etInput);
+        String s = etInput.getText().toString();
+        double cal = Double.parseDouble(s);
+        RadioGroup rgFrom = (RadioGroup)findViewById(R.id.rgFrom);
+        int selFrom = rgFrom.getCheckedRadioButtonId();
+        RadioGroup rgTo = (RadioGroup)findViewById(R.id.rgTo);
+        int selTo = rgTo.getCheckedRadioButtonId();
+        double temp=0;
+        if(selFrom == R.id.rbFrmC) {
+            if (selTo == R.id.rbToC) {
+                temp=cal;
+            } else if (selTo == R.id.rbToF) {
+               temp = cal * (9.0 / 5) + 32;
+
+            } else if (selTo == R.id.rbToK) {
+                temp = cal + 273.15;
+
+            }
+        }
+        else if(selFrom == R.id.rbFrmF){
+                if(selTo == R.id.rbToF){
+                    temp=cal;
+                }
+                else if(selTo == R.id.rbToC) {
+                    temp= (cal-32)*(5.0/9);
+
+                }
+                else if(selTo == R.id.rbToK) {
+                    temp = (cal + 459.67)*(5.0/9);
+
+                }
+
+        }
+        else if(selFrom == R.id.rbFrmK){
+            if(selTo == R.id.rbToK){
+                temp=cal;
+            }
+            else if(selTo == R.id.rbToC) {
+                temp= cal-273.15;
+
+            }
+            else if(selTo == R.id.rbToF) {
+                temp = (cal *(9/5))-459.67;
+
+            }
+
+        }
+        TextView t =(TextView)findViewById(R.id.tvOutput);
+        t.setText(Double.toString(temp));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
